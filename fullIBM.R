2d7d56f7241d5acbf908b5bee7b429f5a17b6961
mbjoseph <maxwellbjoseph@gmail.com>
2013-10-16 11:26:47 -0600

    replaced symbiont invasion from pool with rain_symbionts

--- fullIBM.R
+++ fullIBM.R
@@ -41,7 +41,7 @@ fullIBM <- function(A=100, N=100, ERmin=-30, ERmax=30, Emin=-30, Emax=30,
     # reorder to account for bias towards connected early nodes
     new.order <- sample(dim(m)[1], replace=F)
     m <- m[new.order,new.order]
-    # plot(graph.adjacency(m)) # visualize the contact network
+    plot(graph.adjacency(m)) # visualize the contact network
     
     # Now, identify whether susceptibles have contacted infectious
     # pstate is timestep X site X parasite
@@ -51,12 +51,12 @@ fullIBM <- function(A=100, N=100, ERmin=-30, ERmax=30, Emin=-30, Emax=30,
     Iindivs <- ifelse(class(pstate[t, which(occupancy==1),]) == "array", 
                       which(apply(pstate[t, which(occupancy==1),], 1,sum) > 0),
                       ifelse(sum(pstate[t, which(occupancy==1),]) == 0, 0, 1))
+    transmitted <- array(0, dim=c(A, P))
     if (sum(Iindivs) > 0){ # if there are infectious individuals
       # go through transmission cycle
       Ssites <- which(occupancy == 1)[Sindivs]
       Isites <- which(occupancy == 1)[Iindivs]
       # are there any S rows in the m matrix that contact I columns?
-      transmitted <- array(0, dim=c(A, P))
       if(any(m[Sindivs,Iindivs] == 1)){
         # test whether parasite establishes for each S-I contact
         # for each susceptible host
@@ -85,40 +85,15 @@ fullIBM <- function(A=100, N=100, ERmin=-30, ERmax=30, Emin=-30, Emax=30,
             transmitted[Ssite, ] <- ptrial
           }
         }
-        resolution <- resolve(transmitted, P)
-        
-        # add establishing parasites
-        pstate[t,,] <- pstate[t,,] + resolution
       } 
     }
 
-    # SYMBIONT INVASION FROM REGIONAL POOL #
-    # call to rain_symbionts()
-    # args: sites occupied by hosts, P, pI, t, pPcol (inits$pPcol), 
-    
-    
-    poccupancy <- apply(pstate[t,,], 1, max)
-    occupancy <- apply(state[t,,], 1, max)
-    open.sites <- which(occupancy - poccupancy == 1)
-    if(sum(poccupancy) < sum(occupancy)){ # if not every host is infected
-      empty.hosts <- which(poccupancy == 0 & occupancy == 1)
-      # which parasite species immigrate to each site?
-      immigration <- array(rbinom(length(empty.hosts)*P,
-                                  1, pI), dim=c(length(empty.hosts), P))
-      # which parasite immigrants establish?
-      # first determine which species occur in the empty sites
-      Ssp <- apply(state[t, open.sites,], 1, which.max)
-      Pest <- immigration * inits$pPcol[Ssp, ] # P(symbiont.colonization) = I(attempting to colonize)*Pr(estab)
-      symbiont.colonization <- array(rbinom(length(Pest), 1, c(Pest)),
-                             dim=c(length(empty.hosts), P))
-      
-      # end rain_symbionts()
-      
-      resolution <- resolve(symbiont.colonization, P)
-      
-      # add establishing immigrants
-      pstate[t, empty.hosts, ] <- pstate[t, empty.hosts,] + resolution
-    }
+    # SYMBIONT INVASION FROM REGIONAL POOL # 
+    symbiont.immigration <- rain_symbionts(state, pstate, A, P, 
+                                           pI, t, pPcol = inits$pPcol)
+    symbiont.colonists <- transmitted + symbiont.immigration
+    resolution <- resolve(symbiont.colonists, P)
+    pstate[t, , ] <- pstate[t, , ] + resolution
     
     # HOST RECOVERY / PARASITE DEATH #
     recovery <- array(rbinom(A*N, 1, c(state[t,,])*gamma), dim=c(A, N))
