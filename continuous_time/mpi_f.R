2d205d59eb26ead5e3e70cd404a44029be62dd4d
mbjoseph <maxwellbjoseph@gmail.com>
2014-12-28 10:55:19 -0700

    add parameters for dynamic host community

--- continuous_time/mpi_f.R
+++ continuous_time/mpi_f.R
@@ -1,5 +1,8 @@
 mpi_f <- function(iter=1, nER=1, maxt=10, H=10, nS=10, 
-                  a_pen=0.1, sig.s=10, gamma=0.01, cells=100){
+                  a_pen=0.1, sig.s=10, rs=.1, gamma=0.1, cells=100){
+  source("symb_init.R")
+  source("host_init.R")
+  source("helpers.R")
   # generate environmental ranges
   ER <- sample(seq(1, 100, by=.01), size = nER, replace=FALSE)
   
@@ -18,16 +21,17 @@ mpi_f <- function(iter=1, nER=1, maxt=10, H=10, nS=10,
   }
   
   # initialize parameters to stay constant across runs
-  # (static host community)
-  r <- 0
-  d <- 0
-  c <- 0
+  # (dynamic host community)
+  r <- 1
+  d <- 0.1
+  beta_d <- -1
+  c <- 0.1
   # H <- 10 # host richness (max)
   
   # symbiont params
-  phi <- .5 # contact rate
+  phi <- 5 # contact rate
   # a_pen <- .1 # interspecific penalty
-  rs <- .01 # rain
+  # rs <- .01 # rain
   # nS <- 50 # regional symbiont richness
   # sig.s <- 10 # niche width
   # gamma <- .01 # recovery rate
@@ -48,7 +52,8 @@ mpi_f <- function(iter=1, nER=1, maxt=10, H=10, nS=10,
       transmission_events <- array(0, dim=c(H, H, nS))
       
       # initialize host community
-      state[1, ] <- sample(1:H, size=cells, replace=T)
+      # state[1, ] <- sample(1:H, size=cells, replace=T)
+      hosts <- host_init(cells, H, pcol=.1)
       
       # initialize symbionts and view niches
       symbionts <- symb_init(H, S=nS, 
@@ -56,13 +61,13 @@ mpi_f <- function(iter=1, nER=1, maxt=10, H=10, nS=10,
                              sERmin, sERmax, sig.s)
       
       # store parameters for later extraction
-      pars <- list(r = r, d = d, c = c,
+      pars <- list(r = r, d = d, c = c, beta_d = beta_d,
                    phi = phi, a_pen = a_pen, rs = rs,
                    cells = cells, H=H, nS=nS,
                    gamma= gamma, 
+                   hosts=hosts,
                    symbionts=symbionts)
       
-      maxt <- 10
       t <- 0
       t.int <- 1
       tau <- NULL
@@ -86,7 +91,6 @@ mpi_f <- function(iter=1, nER=1, maxt=10, H=10, nS=10,
         transmission_events <- nextstep$transmission_events
         
         # update time
-        
         t.int <- t.int + 1
         t[t.int] <- t[t.int - 1] + tau[t.int-1]
         setTxtProgressBar(pb, t[t.int])
@@ -106,8 +110,29 @@ mpi_f <- function(iter=1, nER=1, maxt=10, H=10, nS=10,
   return(list(trans_bar = trans_bar, 
               rich_bar = rich_bar, 
               ER = ER, 
-              Earray = Earray)
+              Earray = Earray, 
+              n.ind = n.ind, 
+              s.ind = s.ind,
+              t=t)
   )
 }
 
-testout <- mpi_f(nER=1, iter=1, maxt=500, nS=100, sig.s=5, H=20, cells=1000)
+testout <- mpi_f(nER=1, iter=1, maxt=5, nS=30, sig.s=3, H=10, 
+                 a_pen = .5, cells=400, gamma=0, rs=0.1)
+nsteps <- dim(testout$t)
+
+par(mfrow=c(1, 2))
+plot(x=testout$t, y=testout$s.ind[, 1], type="l", ylim=c(0, max(testout$s.ind)), 
+     xlab="Time", ylab="Number of infected hosts")
+for (k in 1:dim(testout$s.ind)[2]){
+  lines(x=testout$t, y=testout$s.ind[, k], col=k + 2)
+}
+
+plot(x=testout$t, y=testout$n.ind[, 1], type="l", 
+     ylim=c(0, max(testout$n.ind)), 
+     xlab="Time", 
+     ylab="Number of hosts")
+for (k in 1:dim(testout$n.ind)[2]){
+  lines(x=testout$t, y=testout$n.ind[, k], col=k+2)
+}
+par(mfrow=c(1, 1))
