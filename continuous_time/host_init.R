03e6e28870506be76a51589adfe5dc2c57bf796e
mbjoseph <maxwellbjoseph@gmail.com>
2014-12-28 10:54:45 -0700

    same pcol for each host

--- continuous_time/host_init.R
+++ continuous_time/host_init.R
@@ -11,42 +11,9 @@
 ## hniche.d = data frame of host niche data
 ## P.col = cell X host species array of colonization probabilities
 
-host_init <- function(cells, Emin, Emax, H, sig.h){
-  # setup ordered vector of environmental conditions
-  Xe <- sort(runif(cells, Emin, Emax))
-  
-  # assume regional min & max = local min & max
-  ERmin <- Emin 
-  ERmax <- Emax
-  
-  # host optima
-  mu.h <- runif(H, ERmin, ERmax)
-  
-  # host niche width
-  sigma.h <- rep(sig.h, H) 
-  
-  # calculate host Z
-  hZ <- rep(NA, H)
-  for (i in 1:H){
-    integrand <- function(Xe) {
-      exp(-((Xe - mu.h[i]) ^ 2) / (2 * sigma.h[i] ^ 2))
-    }
-    res <- integrate(integrand, lower=ERmin, upper=ERmax)
-    hZ[i] <- 1 / res$value
-  }
-  
-  # host probability of establishment
-  Pcol <- array(dim=c(cells, H))
-  for (i in 1:H){
-    for (j in 1:cells){
-      Pcol[j, i] <- hZ[i] * exp(-((Xe[j] - mu.h[i]) ^ 2) / (2 * sigma.h[i] ^ 2))
-    }
-  }
-  
-  # store host niche data
+host_init <- function(cells, H, pcol){
   host.species <- rep(1:H, each=cells)
-  environmental.condition <- rep(Xe, H)
-  Pr.estab <- c(Pcol)
-  hniche.d <- data.frame(host.species, environmental.condition, Pr.estab)
-  return(list(hniche.d = hniche.d, Pcol = Pcol))
+  environmental.condition <- rep(0, H)
+  Pcol <- array(pcol, dim=c(cells, H))  
+ return(list(Pcol = Pcol))
 }
