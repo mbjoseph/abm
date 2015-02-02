0849e9a9cff1d1e9faeb6d8257b6ea21227227d2
mbjoseph <maxwellbjoseph@gmail.com>
2015-02-01 21:04:43 -0700

    increase maxt, nS, and H

--- continuous_time/janus/runtimes.R
+++ continuous_time/janus/runtimes.R
@@ -1,9 +1,13 @@
 # file to evaluate runtimes for various parameter combinations
-n.iter <- 5
+source("continuous_time/janus/mpi_f.R")
+source("continuous_time/janus/symb_init.R")
+source("continuous_time/janus/host_init.R")
+source("continuous_time/janus/helpers.R")
+n.iter <- 20
 
-H <- 50
-nS <- 50
-maxt <- 20000
+H <- 100
+nS <- 100
+maxt <- 50000
 
 runtimes <- rep(NA, n.iter)
 timesteps <- rep(NA, n.iter)
@@ -11,7 +15,7 @@ tmax <- rep(NA, n.iter)
 
 for (i in 1:n.iter){
   runtimes[i] <- system.time(testout <- mpi_f(iter=1, nER=1, maxt=maxt, H=H, 
-                                              nS=nS, a_pen=1, sig.s=50, rs=.1, 
+                                              nS=nS, a_pen=1, sig.s=3, rs=.01, 
                                               gamma=0, cells=100, r=.4, d=.3, 
                                               beta_d_min = -1, beta_d_max = 1, 
                                               c=.001, phi=5)
